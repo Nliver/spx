79b50108b041ee596c6c12115009a4291321df5b
jiepengtan <jiepengtan@gmail.com>
2025-08-15 18:08:14 +0800

    Fix input error #770

--- internal/enginewrap/extention.go
+++ internal/enginewrap/extention.go
@@ -10,9 +10,15 @@ func callInMainThread(call func()) {
 	mainCallback(call)
 }
 
+const (
+	MOUSE_BUTTON_LEFT   int64 = 1
+	MOUSE_BUTTON_RIGHT  int64 = 2
+	MOUSE_BUTTON_MIDDLE int64 = 3
+)
+
 // =============== input ===================
 func (pself *inputMgrImpl) MousePressed() bool {
-	return inputMgr.GetMouseState(0) || inputMgr.GetMouseState(1)
+	return inputMgr.GetMouseState(MOUSE_BUTTON_LEFT) || inputMgr.GetMouseState(MOUSE_BUTTON_RIGHT)
 }
 
 // =============== window ===================
